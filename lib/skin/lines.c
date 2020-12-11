b834403e531b095c246616378b0247159070a03b
Алексей Резниченко <alex@sochi.biz>
2020-12-11 09:12:53 +0300

    Set space lines

--- lib/skin/lines.c
+++ lib/skin/lines.c
@@ -64,6 +64,8 @@ mc_skin_lines_parse_ini_file (mc_skin_t * mc_skin)
         mc_skin_hardcoded_space_lines (mc_skin);
     else if (mc_global.tty.ugly_line_drawing)
         mc_skin_hardcoded_ugly_lines (mc_skin);
+    
+    mc_skin_hardcoded_space_lines (mc_skin);
 
     /* single lines */
     mc_tty_frm[MC_TTY_FRM_VERT] = mc_skin_lines_load_frm (mc_skin, "vert");
